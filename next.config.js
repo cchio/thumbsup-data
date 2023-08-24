179bee60287c444d85a6f5202d21b524a2fc5ac4
Clarence Chio <chiochio@gmail.com>
2023-08-23 23:33:19 -0600

    Fixed login redirect

--- next.config.js
+++ next.config.js
@@ -8,10 +8,10 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: '/api/:path*',
+        source: '/api/py/:path*',
         destination:
           process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:5328/api/:path*'
+            ? 'http://127.0.0.1:5328/api/py/:path*'
             : '/api/',
       },
     ]
