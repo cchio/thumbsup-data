ec04eba856a310eaed8c7592c9f812f88f917730
Clarence Chio <chiochio@gmail.com>
2023-08-25 00:17:23 -0600

    Reverted to 02a60277fc1a9ebac10afd9148d3314596c314b0

---
+++ app/api/_vercel_kv.py
@@ -0,0 +1,90 @@
+# KV Store 
+# From https://github.com/bestK/python-vercel-kv/blob/main/src/vercel_kv.py
+# Name prefixed with '_' to tell Vercel not to make it a serverless function.
+
+import os
+import requests
+import json
+
+from typing import Optional
+from pydantic import BaseModel
+
+
+class KVConfig(BaseModel):
+    url: str
+    rest_api_url: str
+    rest_api_token: str
+    rest_api_read_only_token: str
+
+
+class Opts(BaseModel):
+    ex: Optional[int]
+    px: Optional[int]
+    exat: None
+    pxat: None
+    keepTtl: None
+
+
+class KV:
+    """
+    Wrapper for https://vercel.com/docs/storage/vercel-kv/rest-api
+    """
+
+    def __init__(self, kv_config: Optional[KVConfig] = None):
+        if kv_config is None:
+            url = os.environ.get("KV_DB_URL")
+            rest_api_url = os.environ.get("KV_DB_REST_API_URL")
+            rest_api_token = os.environ.get("KV_DB_REST_API_TOKEN")
+            rest_api_read_only_token = os.environ.get("KV_DB_REST_API_READ_ONLY_TOKEN")
+            if None in [url, rest_api_url, rest_api_token, rest_api_read_only_token]:
+                raise Exception("_vercel_kv class missing required env variables.")
+            
+            self.kv_config = KVConfig(
+                url=url,
+                rest_api_url=rest_api_url,
+                rest_api_token=rest_api_token,
+                rest_api_read_only_token=rest_api_read_only_token,
+            )
+        else:
+            self.kv_config = kv_config
+
+    def get_kv_conf(self) -> KVConfig:
+        return self.kv_config
+
+    def has_auth(self) -> bool:
+        kv_config = KV.get_kv_conf()
+        headers = {
+            'Authorization': f'Bearer {self.kv_config.rest_api_token}',
+        }
+        resp = requests.get(self.kv_config.rest_api_url, headers=headers)
+        return resp.json()['error'] != 'Unauthorized'
+
+    def set(self, key, value, opts: Optional[Opts] = None) -> bool:
+        headers = {
+            'Authorization': f'Bearer {self.kv_config.rest_api_token}',
+        }
+
+        url = f'{self.kv_config.rest_api_url}/set/{key}/{value}'
+
+        if opts is not None and opts.ex is not None:
+            url = f'{url}/ex/{opts.ex}'
+
+        resp = requests.get(url, headers=headers)
+        return resp.json()['result']
+
+    def set_json(self, key, value) -> bool:
+        headers = {
+            'Authorization': f'Bearer {self.kv_config.rest_api_token}',
+        }
+
+        url = f'{self.kv_config.rest_api_url}/set/{key}'
+        resp = requests.post(url, data=json.dumps(value), headers=headers)
+        return resp.json()['result']
+
+    def get(self, key) -> bool:
+        headers = {
+            'Authorization': f'Bearer {self.kv_config.rest_api_token}',
+        }
+
+        resp = requests.get(f'{self.kv_config.rest_api_url}/get/{key}', headers=headers)
+        return resp.json()['result']
---
+++ app/api/_vercel_pg.py
@@ -0,0 +1,39 @@
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+import json
+import os
+import psycopg2
+import sys
+
+class PG:
+
+    def __init__(self):
+        host = os.environ.get('POSTGRES_HOST')
+        dbname = os.environ.get('POSTGRES_DATABASE')
+        user = os.environ.get('POSTGRES_USER')
+        password = os.environ.get('POSTGRES_PASSWORD')
+        port = os.environ.get('POSTGRES_PORT')
+        if None in [host, dbname, port]:
+            raise Exception("_vercel_pg class missing required env variables.")
+
+        self.connection = psycopg2.connect(
+            host = host,
+            dbname = dbname,
+            user = user,
+            password = password,
+            port = port,
+        )
+        self.connection.autocommit = True
+        self.database_url = f'postgresql://{user}:{password}@{host}:{port}/{dbname}'
+
+
+    def write_df_to_table(self, table_name, df):
+        df.to_sql(table_name, self.database_url, if_exists='replace')
+
+
+    def get_table_schema(self, table_name):
+        cur = self.connection.cursor()
+        cur.execute(f'select * from information_schema.columns where table_schema = \'public\' and table_name = \'{table_name}\';')
+        res = cur.fetchall()
+        cur.close()
+        return res
