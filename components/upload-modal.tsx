be8c89f26abfac49bfa18de925eff16121372481
Clarence Chio <chiochio@gmail.com>
2023-08-26 18:17:42 -0600

    linked upload file button to api

--- components/upload-modal.tsx
+++ components/upload-modal.tsx
@@ -1,35 +1,66 @@
 'use client'
 
-import React, { useState } from "react";
-import { Modal, useModal } from '@geist-ui/core'
-import { cn } from '@/lib/utils'
 import { buttonVariants } from '@/components/ui/button'
+import { cn, humanFileSize } from '@/lib/utils'
 import { FileText } from '@geist-ui/icons'
 import { FileUploader } from "react-drag-drop-files";
+import { Modal, useModal } from '@geist-ui/core'
+import { toast } from 'react-hot-toast'
+import React, { useState } from "react";
 
 const fileTypes = ["TXT", "CSV"];
  
 export default function UploadModal() {
   const { visible, setVisible, bindings } = useModal()
 
-  const [file, setFile] = useState(null)
+  const [file, setFile] = useState<File>()
 
-  const handleChange = (file:any) => {
-    setFile(file);
+  const handleChange = (f:any) => {
+    setFile(f);
   };
+
+  const handleSubmit = async () => {
+try {
+      const data = new FormData()
+      data.set('file', (file as any))
+
+      const res = await fetch('/api/table/add', {
+        method: 'POST',
+        body: data
+      })
+
+      // Handle errors
+      if (!res.ok) {
+        toast.error(await res.text());
+      }
+    } catch (e: any) {
+      toast.error('Encountered unknown error')
+      console.error(e)
+    }
+
+    setVisible(false);
+    handleChange(null);
+    toast.success(`Successfully uploaded ${(file as any).name}`)
+  }
  
   return (
     <>
       <Modal {...bindings}>
-        <Modal.Title>Upload CSVs here</Modal.Title>
+        <Modal.Title>Upload CSV</Modal.Title>
         <Modal.Content>
           <FileUploader handleChange={handleChange} name="file" types={fileTypes} />
+            <p className="pt-4 text-xs text-muted-foreground">
+              { file == null ? '' : `File uploaded: ${(file as any).name} (${humanFileSize((file as any).size)})` }
+            </p>
         </Modal.Content>
-        <Modal.Action passive onClick={() => setVisible(false)}>Cancel</Modal.Action>
-        <Modal.Action>Submit</Modal.Action>
+        <Modal.Action passive onClick={ () => setVisible(false) }>Cancel</Modal.Action>
+        <Modal.Action onClick={ () => handleSubmit() }>Submit</Modal.Action>
       </Modal>
       <a
-        onClick={() => setVisible(true)}
+        onClick={() => {
+          setVisible(true);
+          handleChange(null);
+        }}
         className={cn(buttonVariants())}
       >
         <FileText size={16}  className="mr-2" />
--- lib/utils.ts
+++ lib/utils.ts
@@ -42,12 +42,34 @@ export function formatDate(input: string | number | Date): string {
   })
 }
 
-
 export function getUserKVKey(userId: string, elementName: string): string {
   return `u:${userId}:${elementName}`
- }
- 
- export function getUserTableName(userId: string, fileName: string): string {
-   return `${userId}__${fileName}`
- }
+}
 
+export function getUserTableName(userId: string, fileName: string): string {
+  return `${userId}__${fileName}`
+}
+
+export function humanFileSize(bytes: number, si = false, dp = 1) {
+  const thresh = si ? 1000 : 1024
+
+  if (Math.abs(bytes) < thresh) {
+    return bytes + ' B'
+  }
+
+  const units = si
+    ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+    : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
+  let u = -1
+  const r = 10 ** dp
+
+  do {
+    bytes /= thresh
+    ++u
+  } while (
+    Math.round(Math.abs(bytes) * r) / r >= thresh &&
+    u < units.length - 1
+  )
+
+  return bytes.toFixed(dp) + ' ' + units[u]
+}
