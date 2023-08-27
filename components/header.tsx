c74f5ec8217b94c185f4e83b347d13b4e8b10d36
Clarence Chio <chiochio@gmail.com>
2023-08-26 22:10:05 -0600

    Updated tailwind css order to silence warnings

--- components/header.tsx
+++ components/header.tsx
@@ -17,10 +17,10 @@ import { UserMenu } from '@/components/user-menu'
 export async function Header() {
   const session = await auth()
   return (
-    <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
+    <header className="sticky top-0 z-50 flex h-16 w-full shrink-0 items-center justify-between border-b bg-gradient-to-b from-background/10 via-background/50 to-background/80 px-4 backdrop-blur-xl">
       <div className="flex items-center">
         <div className="flex items-center">
-          <IconSeparator className="w-6 h-6 text-muted-foreground/50" />
+          <IconSeparator className="h-6 w-6 text-muted-foreground/50" />
           {session?.user ? (
             <UserMenu user={session.user} />
           ) : (
@@ -38,7 +38,7 @@ export async function Header() {
           className={cn(buttonVariants({ variant: 'outline' }))}
         >
           <IconGitHub />
-          <span className="hidden ml-2 md:flex">Open in GitHub</span>
+          <span className="ml-2 hidden md:flex">Open in GitHub</span>
         </a>
         <UploadModal/>
         <ListUploadsModal/>
